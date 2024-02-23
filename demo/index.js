620272723f1a4c4c820fe376c9eb774ed97e2edb
Jason Baker <jason@capsule42.com>
2024-02-22 19:32:57 -0800

    perf(examples): update to latest example syntax

--- demo/index.js
+++ demo/index.js
@@ -1,15 +1,18 @@
 // import { exampleFunction } from "exampleFile.js";
 
-export function initSelectIndexExamples(initCount) {
+/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
+
+export function initExamples(initCount) {
   initCount = initCount || 0;
 
   try {
+    // javascript example function calls to be added here upon creation to test examples
     // exampleFunction();
-  } catch {
+  } catch (err) {
     if (initCount <= 20) {
       // setTimeout handles issue where content is sometimes loaded after the functions get called
       setTimeout(() => {
-        initSelectIndexExamples(initCount + 1);
+        initExamples(initCount + 1);
       }, 100);
     }
   }

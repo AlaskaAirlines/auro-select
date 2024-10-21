59b4666ef04603cd5a516b1563761386f32dd8d1
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-21 14:25:21 -0700

    docs(links): remove unused CDN links in demo files
    removed `auro-icon`, `auro-button`, etc from `index.html` and `api.html`

--- demo/api.js
+++ demo/api.js
@@ -4,6 +4,10 @@ import { valueExample } from "../apiExamples/value";
 import { valueExtractionExample } from "../apiExamples/valueExtraction";
 
 /* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
+import { AuroSelect } from '../src/auro-select.js';
+import '@aurodesignsystem/auro-menu';
+
+AuroSelect.register(); // registering to `auro-select`
 
 export function initExamples(initCount) {
   initCount = initCount || 0;
--- demo/index.js
+++ demo/index.js
@@ -3,6 +3,7 @@
 /* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
 
 import { AuroSelect } from '../src/auro-select.js';
+import '@aurodesignsystem/auro-menu';
 
 AuroSelect.register(); // registering to `auro-select`
 AuroSelect.register('custom-select');
